7dac33105e52bb3790319d6769bd9d9feba9c04a
gomezcalderondaniel <42280710+gomezcalderondaniel@users.noreply.github.com>
2023-05-11 07:32:14 -0500

    Update script.js

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ const minutesEl = document.getElementById("minutes");
 const secondsEl = document.getElementById("seconds");
 
 
-const bday = "10 Jan 2023";
+const bday = "10 Jan 2024";
 
 
 var Input = {
